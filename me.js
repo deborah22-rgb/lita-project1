1f157ccae9bce901cc14593723d37e56f9527cc6
deborah22-rgb <basseydeboraheffiong@gmail.com>
2024-12-19 01:13:05 -0800

    Add files via upload
    initial commit

---
+++ me.js
@@ -0,0 +1,15 @@
+const navlink=document.querySelectorAll('.nav-links li');
+const ment = document.querySelector('.moblie-menu-btn');
+const qwer = document.querySelector('.moblie-menu');
+const overflow = document.querySelector('overflow');
+const moblielinks = document.querySelectorAll('.moblie-links li');
+
+ment.addEventListener("click",()=> {
+    const icon=document.querySelector[".moblie-menu-btn img"];
+    ment.classList.toggle("active-btn");
+    mobliemenu.classList.toggle("active-menu");
+    overflow.classList.toggle("active-menu");
+    if (ment.classList.contains("active-btn"));
+}
+)
+   
